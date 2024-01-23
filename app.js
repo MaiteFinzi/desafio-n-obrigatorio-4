e91445752dd84d3178a3c652b0759383b265cd18
Maite Finzi de Sousa Buiatti <106894876+MaiteFinzi@users.noreply.github.com>
2024-01-23 14:03:02 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+nomes = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+console.log(nomes[2]);
+
+nomes = ['JavaScript', 'Python', 'Go'];
+console.log(nomes[0]);
+
+nomes = ['JavaScript', 'Python', 'Go'];
+console.log(nomes[1]);
+
+nomes = ['JavaScript', 'Python', 'Go'];
+console.log(nomes[2]);
+
+const linguagens = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+linguagens.push('Java', 'Ruby', 'GoLang');
+console.log(linguagens); 
